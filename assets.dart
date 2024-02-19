dbb6a82aaebcb4f6a7acf760965e36460a599eb1
Jamal <33414974+jamalTek@users.noreply.github.com>
2024-02-19 21:21:05 +0400

    Add files via upload

---
+++ assets.dart
@@ -0,0 +1,85 @@
+// Paths
+
+const String animationPath = 'assets/animations';
+const String imagesPath = 'assets/images';
+const String iconsPath = 'assets/icons';
+
+// Animations
+
+const String aniPhoneAndFood = '$animationPath/phone_and_food.json';
+const String aniFood = '$animationPath/food.json';
+const String aniPersonalTrainer = '$animationPath/personal_trainer.json';
+const String aniWalingBall = '$animationPath/walking_man.json';
+const String aniLoading = '$animationPath/loading.json';
+const String aniDone = '$animationPath/done.json';
+const String aniFuture = '$animationPath/future.json';
+const String aniExplore = '$animationPath/explore.json';
+const String aniInfo = '$animationPath/info.json';
+const String aniDelete = '$animationPath/delete.json';
+const String aniDeleted = '$animationPath/deleted.json';
+const String aniDoctor = '$animationPath/doctor.json';
+const String aniHello = '$animationPath/hello.json';
+const String aniReminder = '$animationPath/reminder.json';
+const String aniPremium = '$animationPath/premium.json';
+const String aniMesh = '$animationPath/mesh.json';
+const String aniHappyStar = '$animationPath/happy_star.json';
+const String aniWinner = '$animationPath/winner.json';
+
+// Images
+
+const String imageWeightLifting = '$imagesPath/weightlifting.png';
+const String imageDoctor = '$imagesPath/doctor.png';
+const String imageLogo = '$imagesPath/logo.png';
+const String imageHeader = '$imagesPath/header.png';
+
+// ***** End of Images **** //
+
+// Icons
+
+const String iconLogout = '$iconsPath/sign_out.svg';
+const String iconQuote = '$iconsPath/quote.svg';
+const String iconFemale = '$iconsPath/female.svg';
+const String iconMale = '$iconsPath/male.svg';
+const String iconFav = '$iconsPath/fav.svg';
+const String iconBreakfast = '$iconsPath/breakfast.svg';
+const String iconLunch = '$iconsPath/lunch.svg';
+const String iconDinner = '$iconsPath/dinner.svg';
+const String iconSnacks = '$iconsPath/snacks.svg';
+const String iconBreakfastDark = '$iconsPath/breakfast_dark.svg';
+const String iconLunchDark = '$iconsPath/lunch_dark.svg';
+const String iconDinnerDark = '$iconsPath/dinner_dark.svg';
+const String iconSnacksDark = '$iconsPath/snacks_dark.svg';
+const String iconBreakfastCircle = '$iconsPath/bf_circle.svg';
+const String iconLunchCircle = '$iconsPath/ln_circle.svg';
+const String iconDinnerCircle = '$iconsPath/dn_circle.svg';
+const String iconSnacksCircle = '$iconsPath/sk_circle.svg';
+const String iconBreakfastGrey = '$iconsPath/bf_grey.svg';
+const String iconLunchGrey = '$iconsPath/ln_grey.svg';
+const String iconDinnerGrey = '$iconsPath/dn_grey.svg';
+const String iconSnacksGrey = '$iconsPath/sk_grey.svg';
+const String iconHome = '$iconsPath/home.svg';
+const String iconRecipes = '$iconsPath/recipes.svg';
+const String iconSettings = '$iconsPath/settings.svg';
+const String iconStatistics = '$iconsPath/statistics.svg';
+const String iconWorkout = '$iconsPath/workout.svg';
+const String iconEdit = '$iconsPath/edit.svg';
+const String iconDottedCircle = '$iconsPath/dotted_circle.svg';
+const String iconNotifications = '$iconsPath/notifications.svg';
+const String iconBodybuilding = '$iconsPath/bodybuilding.svg';
+const String iconCardio = '$iconsPath/cardio.svg';
+const String iconTheme = '$iconsPath/theme.svg';
+const String iconGoal = '$iconsPath/goal.svg';
+const String iconStretching = '$iconsPath/stretching.svg';
+const String iconAddMealButton = '$iconsPath/add_meal_button.svg';
+const String iconFoodDrink = '$iconsPath/food_drink.svg';
+const String iconLogo = '$iconsPath/logo.svg';
+const String iconChield = '$iconsPath/chield.svg';
+const String iconPeople = '$iconsPath/people.svg';
+const String iconPremium = '$iconsPath/premium.svg';
+const String iconPremiumBig = '$iconsPath/premium_big.svg';
+const String iconFire = '$iconsPath/fire.svg';
+const String iconTarget = '$iconsPath/target-arrow.svg';
+
+// Svg shapes
+
+const String svgHeader = 'assets/icons/header_shape.svg';
